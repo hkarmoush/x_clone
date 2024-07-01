131907739267095d870259dad72fbbeb9db7c9f4
Hasan Armoush <hkarmoush@gmail.com>
2024-07-01 22:12:32 +0300

    registration details uses freezed

--- lib/app/domain/entities/auth/registration_details.dart
+++ lib/app/domain/entities/auth/registration_details.dart
@@ -1,10 +1,12 @@
-class RegistrationDetails {
-  RegistrationDetails({
-    required this.name,
-    required this.email,
-    required this.password,
-  });
-  final String name;
-  final String email;
-  final String password;
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'registration_details.freezed.dart';
+
+@freezed
+class RegistrationDetails with _$RegistrationDetails {
+  const factory RegistrationDetails({
+    required String name,
+    required String email,
+    required String password,
+  }) = _RegistrationDetails;
 }
